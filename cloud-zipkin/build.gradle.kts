5676d4d18140cfd7abfc8e3281fb4a208341203a
daycold <970540420@qq.com>
2020-04-03 18:57:24 +0800

    use undertow

--- cloud-zipkin/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-dependencies {
-    compile("io.zipkin.java:zipkin-server") 
-    compile("io.zipkin.java:zipkin-autoconfigure-ui")
-    compile("org.springframework.boot:spring-boot-starter-undertow")
-}
