861b732bf81623eec668e93d11011f95c2b8c503
daycold <970540420@qq.com>
2020-04-22 16:54:13 +0800

    build jar

---
+++ cloud-ktor/src/main/kotlin/com/demo/ktor/KtorApplication.kt
@@ -0,0 +1,9 @@
+package com.demo.ktor
+
+/**
+ * @author Stefan Liu
+ */
+class KtorApplication
+
+fun main(args: Array<String>) {
+}
--- cloud-web/build.gradle.kts
+++ cloud-web/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
-//    implementation("org.springframework.boot:spring-boot-starter-tomcat:${project.extra["springBootVersion"]}")
     compile("org.springframework.boot:spring-boot-starter-undertow")
     compile("org.springframework.boot:spring-boot-starter-web")
 }
